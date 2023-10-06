baa7f5833133ab316df45e782ff61e76c66ff396
suvasis999 <talk2suvasis@gmail.com>
2023-10-06 12:21:54 +0530

    updates

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
 
 app.use('/api', eventRoutes.routes);
 
 
 
-app.listen(config.port, () => {
+/*app.listen(config.port, () => {
   console.log('app listening on url http://localhost:' + config.port )
+});*/
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send(`Working fine on server ${config.port}`);
 });
+
+app.listen(config.port, () => {
+  console.log("Server is up on port " + config.port);
+});
+
+module.exports = app;
